eea1f1097c63ca4e9afb826d10f2a830a204f5a9
Opilane TTHK <opilane@tthk.ee>
2021-09-15 12:19:27 +0300

    first commit

---
+++ week3exercises/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace week3exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tingimused sisseastumiseks
+            //miinimum matemaamtikas 85 punkti
+            //miinimum keemias 90 punkti
+            //miinimum bioloogias 95 punkti
+            //programm küsib kasutajal sisestada tema
+            //eksami punktid ja otsustab, kas kasutaja
+            //saab arstiks õppida
+
+            Console.WriteLine("Sisesta matemaatika eksami tulemus:");
+            int math = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sisesta keemia eksami tulemus:");
+            int chemistry = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sisesta bioloogia eksami tulemus:");
+            int biology = Convert.ToInt32(Console.ReadLine());
+
+            if (math >= 85 && chemistry >= 90 && biology >= 95)
+            {
+                Console.WriteLine("Tubli! saad arstiks õppida.");
+            }
+            else
+            {
+                Console.WriteLine("Proovi järgmine aasta.");
+            }
+
+
+
+        }
+    }
+}
